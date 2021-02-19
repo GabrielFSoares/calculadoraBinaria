61661dab5f96736b05a3bd481ab95806f85ecbad
Your Name <you@example.com>
2021-02-18 23:18:29 -0300

    Revert "fin"
    This reverts commit 2dfed89eff0f323ff9b6b368cb22a58eb92287c4.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,11 @@
-import { IntegrantesComponent } from './components/integrantes/integrantes.component';
-import { HistoricoComponent } from './components/historico/historico.component';
-import { CalculadoraComponent } from './components/calculadora/calculadora.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [
-  { path: '', component: CalculadoraComponent },
-  { path: 'calculadora', component: CalculadoraComponent},
-  { path: 'historico', component: HistoricoComponent },
-  { path: 'integrantes', component: IntegrantesComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,18 @@
-import { HistoricoService } from './services/historico.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CalculadoraComponent } from './components/calculadora/calculadora.component';
-import { TopnavComponent } from './components/nav/topnav/topnav.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MenuComponent } from './components/nav/menu/menu.component';
-import { HistoricoComponent } from './components/historico/historico.component';
-import { IntegrantesComponent } from './components/integrantes/integrantes.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CalculadoraComponent,
-    TopnavComponent,
-    MenuComponent,
-    HistoricoComponent,
-    IntegrantesComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FontAwesomeModule,
-    BrowserAnimationsModule
+    AppRoutingModule
   ],
-  providers: [HistoricoService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
